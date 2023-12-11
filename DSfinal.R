65d6724d4e2545d48d8f932af7c9fb2f11ca8f04
Ahmed <ahmed@gmail.com>
2023-12-11 21:38:07 +0200

    clean data

--- DSfinal.R
+++ DSfinal.R
@@ -1,10 +1,22 @@
+library(tidyr)
 data <- read.csv("games.csv", header = TRUE)
 head(data)
 duplicated(data)
 is.na(data)
 str(data)
 
-game_moves <- data["moves"]
-
+game_moves <- data$moves
+players <- c(data$white_id, data$black_id)
+date_of_games <- data$created_at
+# remove unrelated data
 data$moves <- NULL
-# 
+data$id <- NULL
+data$black_id <- NULL
+data$white_id <- NULL
+data$last_move_at <- NULL
+data$created_at <- NULL
+str(data)
+# slicing main opening names
+data <- separate(data, opening_name, into = c("main_opening", "variation"), sep = ":")
+data$variation <- NULL
+str(data)
