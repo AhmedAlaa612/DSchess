e0720f6a300aa81b43c31285fd332b0bc8126b46
Ahmed <ahmed@gmail.com>
2023-12-12 23:10:54 +0200

    clustering data ready

--- DSfinal.R
+++ DSfinal.R
@@ -15,8 +15,36 @@ data$black_id <- NULL
 data$white_id <- NULL
 data$last_move_at <- NULL
 data$created_at <- NULL
+data$opening_eco <- NULL
 str(data)
 # slicing main opening names
-data <- separate(data, opening_name, into = c("main_opening", "variation"), sep = ":")
+data <- separate(data, opening_name, into = c("main_opening", "variation"), sep = ":", extra = "merge", fill = "right")
 data$variation <- NULL
 str(data)
+data$variation <- NULL
+# slicing time
+data <- separate(data, increment_code, into = c("time_control", "increment"), sep = "\\+")
+data$increment_code <- NULL
+data$time_control <- as.numeric(data$time_control)
+data$increment <- as.numeric(data$increment)
+###########################################################################
+#data for clustering
+library(caret)
+library(dplyr)
+clustering_data <- data
+# rated (TRUE, FALSE) -> 1, 0
+clustering_data$rated <- ifelse(data$rated == "TRUE", 1, 0)
+# count encode opening names
+counts <- table(clustering_data$main_opening)
+clustering_data <- clustering_data %>%
+  mutate(opening = counts[main_opening])
+clustering_data$opening <- as.numeric(clustering_data$opening)
+clustering_data$main_opening <- NULL
+#create dummy variables for victory status and winner
+encoded_data <- dummyVars(formula = ~victory_status+winner-1, data =  clustering_data, sep = '_')
+encoded_df <- data.frame(predict(encoded_data, newdata = data))
+str(encoded_df)
+final_clustering_data = cbind(clustering_data, encoded_df)
+final_clustering_data$victory_status <- NULL
+final_clustering_data$winner <- NULL
+
